fcd0027e31649bc26ddf7a9d6c61bcdd1e3e714b
Danila Aleksin <danya09kz@gmail.com>
2023-06-14 03:26:43 +0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -9,9 +9,9 @@ module.exports = ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'p-336138_strapi'),
+        user: env('DATABASE_USERNAME', 'p-336138_strapi'),
+        password: env('DATABASE_PASSWORD', '9k^cR7m36'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
