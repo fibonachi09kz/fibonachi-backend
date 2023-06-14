dddb13c1def3b436359bd8e512c7ac11eef238e2
fibonachi09kz <69359758+fibonachi09kz@users.noreply.github.com>
2023-06-14 17:23:49 +0600

    aa

--- config/database.js
+++ config/database.js
@@ -20,7 +20,7 @@ module.exports = ({ env }) => {
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool(
             'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            false
+            true
           ),
         },
       },
@@ -67,7 +67,7 @@ module.exports = ({ env }) => {
           ),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
-      },
+	},	
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     sqlite: {
